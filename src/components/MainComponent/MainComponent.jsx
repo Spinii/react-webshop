bee8c2ce9ea28175629065e5513770b0a2c4d4dd
VPapic <vjekopapic55@gmail.com>
2024-03-12 09:51:46 +0100

    sredivanje landing pagea

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,26 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./MainComponent.css"
+import { RiSkipLeftLine, RiSkipRightLine } from "@remixicon/react";
 
 function MainComponent(){
 
-    const products = [
-        {
-            title: "Title 1",
-            description: "Description 1"
-        },
-        {
-            title: "Title 2",
-            description: "Description 2"
-        },
-        {
-            title: "Title 3",
-            description: "Description 3"
-        },
-        {
-            title: "Title 4",
-            description: "Description 4"
-        }
-    ]
+    const [fetchedProducts, setFetchedProducts] = useState(null);
+
+    const [titles, setTitles] = useState([]);
+
+    const [description, setDescription] = useState([]);
+
+    const [images, setImages] = useState([]);
+
+    const [coverImage, setCoverImage] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('https://dummyjson.com/products');
+                const data = await response.json();
+                console.log("Fetched Products:", data);
+
+                
+                const productTitles = data.products.map(product => product.title);
+                const productDescription = data.products.map(product => product.description);
+                const productImages = data.products.map(product => product.images);
+                const firstImage = productImages.map(image => image[0]);
+                setTitles(productTitles);
+                setDescription(productDescription)
+                setImages(productImages);
+                setCoverImage(firstImage);
+                console.log("Titles:", productTitles);
+                console.log("Description:", productDescription);
+                console.log("Images:", productImages);
+                console.log("firstimage => ", firstImage)
+               
+                
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+
 
     const [productCount, setProductCount] = useState(0);
 
@@ -28,38 +52,37 @@ function MainComponent(){
 
         setProductCount(productCount - 1)
         if(productCount === 0){
-            setProductCount(3)
+            setProductCount(5)
         }
     }
 
     function handleNextProduct(){
 
         setProductCount(productCount + 1)
-        if(productCount === 3){
+        if(productCount === 5){
             setProductCount(0)
         }
     }
 
-    console.log(products[1].title);
-
 
 
     return(
-        <>  
+        <>
         <div className="main-container">
             <div onClick={() => handlePreviousProduct()} className="main-container-btn">
-                Prev
+                <RiSkipLeftLine color="white"/>
             </div>
+            { titles && 
             <div className="main-container-text">
-                <h4>{products[productCount].title}</h4>
-                <p>{products[productCount].description}</p>
-            </div>
+                 <h4>{titles[productCount]}</h4>
+                <p>{description[productCount]}</p>
+            </div>}
             <div className="main-container-product">
-                <h2>Product Image</h2>
+                <img src={coverImage[productCount]}></img>
             </div>
             <div onClick={() => handleNextProduct()} className="main-container-btn">
-                Next
-            </div>
+                <RiSkipRightLine color="white"/>
+            </div> 
         </div>
         </>
     )
